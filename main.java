ba37e6107a0a1fc654ad2fc1ed3000a1b1ace75e
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-01 13:25:09 +0530

    main file

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        coupanCode.check();
+
+        fibonachiSeries.check();
+
+        perfectNumber.check();
+
+        primeNumber.chack();
+    }
+}
